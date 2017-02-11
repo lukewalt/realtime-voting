ba27572415901c7df185399acf777000fe2993d3
Luke Woodward <luke.wdwrd@gmail.com>
2017-02-11 11:44:37 -0600

    refactore message logic

--- app.js
+++ app.js
@@ -1,64 +1,115 @@
-{
+{// wraps the code to prevent the creation of global variables... by creating a "block"
 
-    firebase.initializeApp({
-      apiKey: 'AIzaSyB-pb00SC2mOtYzia0c5ujquMuH-lek3Yc',
-      authDomain: 'realtime-voting.firebaseapp.com',
-      databaseURL: 'https://realtime-voting.firebaseio.com',
-      storageBucket: 'realtime-voting.appspot.com',
-      messagingSenderId: '485500242156'
-    });
+firebase.initializeApp({
+  apiKey: 'AIzaSyB-pb00SC2mOtYzia0c5ujquMuH-lek3Yc',
+  authDomain: 'realtime-voting.firebaseapp.com',
+  databaseURL: 'https://realtime-voting.firebaseio.com',
+  storageBucket: 'realtime-voting.appspot.com',
+  messagingSenderId: '485500242156'
+});
 
+  const MAX_MESSAGES_DISPLAYED = 10
 
+  const messagesRef = firebase.database().ref('messages')
+  const messagesDiv = document.querySelector('.messages')
+  const form = document.querySelector('form')
 
+  const votesRef = firebase.database().ref('votes')
+  const voteButtons = document.querySelectorAll('.choice button')
+  const voteTotals = document.querySelectorAll('.vote_totals')
 
-    document
-        .querySelectorAll('.choice button')
-        .forEach(b => b.addEventListener('click', onVote));
+  const sendVote = (evt) => {
+    // what button i clicked on
+    const voteFor = evt.target.closest('.choice').dataset.value
 
-    //function for click event
-    function onVote(e) {
-        //submit vote
-        // //what button
-        const url = 'https://realtime-voting.firebaseio.com/votes.json'
-        const voteFor = e.target.closest('.choice').dataset.value
+    // go get the current counts
+    votesRef.once('value')
+      .then(snap => snap.val())
+      .then(data => {
+        // update the new count
+        const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1
+        votesRef.update({ [voteFor]: newCount })
+      })
+      .catch(console.error)
 
-        //go get the current count
-        firebase.database().ref('votes').once('value')
-        .then((snap) => snap.val())
-        .then(data => {
-            //tertiary if else
-            const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1
+    voteButtons.forEach(btn => btn.remove())
+    voteTotals.forEach(item => item.classList.remove('hidden'))
+  }
 
-            //patch data //happening over the socet in replacement of patch
-            return firebase.database().ref('votes').update({ [voteFor] : newCount })
-        })
+  const sendMessage = (evt) => {
+    evt.preventDefault()
 
-        //// show current vote total
-        .catch(console.error)
+    const nameInput = evt.target.elements.name // evt.target.querySelctor('[name="name"]')
+    const contentInput = evt.target.elements.content // evt.target.querySelctor('[name="content"]')
 
-        document.querySelectorAll('button').forEach(btn => btn.remove())
-        document.querySelectorAll('.hidden').forEach(item => item.classList.remove('hidden'))
+    const name = nameInput.value.trim()
+    const content = contentInput.value.trim()
+
+    if (name && content) {
+      messagesRef.push({ name, content })
+        .then(() => contentInput.value = '')
     }
+  }
 
-    firebase.database().ref('votes').on('value', onUpdate)
+  const onNewVote = (snap) => {
+    const votes = snap.val()
 
+    document.querySelectorAll('h3').forEach(voteTotal => {
+      const choice = voteTotal.closest('.choice').dataset.value
+      const total = Object.values(votes).reduce((acc, val) => acc + val) // SUM
+      const current = votes[choice]
 
-    function onUpdate (snap) {
-        const data = snap.val()
+      voteTotal.innerText = Math.round(current / total * 100) + '%'
+    })
+  }
 
-        document.querySelectorAll('h3').forEach(h =>{
-            //calc total
-            const total = Object.values(data).reduce((acc, val) => acc + val);
-            const current = data[h.closest('.choice').dataset.value];
-            //update dom
-            h.innerText = Math.round(current/total * 100) + "%"
-        })
+  const renderMessage = (msg) => {
+    const docFragment = document.createDocumentFragment()
 
+    const div = document.createElement('DIV')
+    docFragment.appendChild(div)
+
+    const strong = document.createElement('STRONG')
+    div.appendChild(strong)
+
+    const name = document.createTextNode(msg.name)
+    strong.appendChild(name)
+
+    const seperator = document.createTextNode(': ')
+    div.appendChild(seperator)
+
+    const span = document.createElement('SPAN')
+    div.appendChild(span)
+
+    const content = document.createTextNode(msg.content)
+    span.appendChild(content)
+
+    return docFragment
+  }
+
+  const onNewMessage = (snap) => {
+    const msg = snap.val() // { name: 'Scott', content: 'Hey' }
+
+    messagesDiv.appendChild(renderMessage(msg))
+
+    // messagesDiv.innerHTML += `
+    //   <div>
+    //     <strong>${msg.name}</strong>: <span>${msg.content}</span>
+    //   </div>
+    // `
+
+    if (messagesDiv.childElementCount > MAX_MESSAGES_DISPLAYED) {
+      messagesDiv.firstChild.remove()
     }
+  }
 
+  form.addEventListener('submit', sendMessage)
+  messagesRef.limitToLast(MAX_MESSAGES_DISPLAYED).on('child_added', onNewMessage)
+
+  voteButtons.forEach(btn => btn.addEventListener('click', sendVote))
+  votesRef.on('value', onNewVote)
 
 }
-
 
 
 
